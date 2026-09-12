d445fa11a29752822b1cae6a781aa8d6760438e2
ManJin03 <ManJin_03@163.com>
2026-09-12 16:17:35 +0800

    feat:破坏性重构，调整代码结构，采用单例类实现，模块化结构

--- main.cpp
+++ main.cpp
@@ -1,51 +1,141 @@
 //
 // Created by 33550 on 2026/8/31.
 //
-#include <cstring>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unistd.h>
 #include <vector>
 #include <sys/wait.h>
+#include <cstring>
+
 using std::cout;
 using std::cin;
-using std::endl;
 using std::string;
+using std::vector;
+using COMMAND_T = vector<char *>;
 
-static void call(const string &input)
+namespace
 {
-    if (const int rc = fork(); rc < 0) {
-        perror("fork error");
-        exit(-1);
-    }
-    else if (rc == 0) {
-        std::stringstream ss(input);
-        string line;
-        std::vector<char *> tokens;
-        while (ss >> line) {
-            char *token = strdup(line.c_str());
-            tokens.push_back(token);
+    class miniShell
+    {
+    public:
+        static miniShell &init(int argc, char *argv[]);
+
+        void run();
+
+        miniShell(const miniShell &) = delete;
+
+        miniShell &operator=(const miniShell &) = delete;
+
+    private:
+        void getInput();
+
+        void setCommand();
+
+        void executeCommands() const;
+
+        void executeCommand(const COMMAND_T &command) const;
+
+        void setPrompt(string prompt = "$miniShell>");
+
+        void setPath(string path);
+
+        void printPrompt() const;
+
+        static void occurError(const char *message);
+
+        miniShell() = default;
+
+        ~miniShell() = default;
+
+        string m_path{"/"};
+        string m_prompt{};
+        string m_input{};
+        vector<COMMAND_T> m_commands{};
+        bool m_redirect{false};
+        bool m_pipe{false};
+        bool m_exit{false};
+    };
+}
+
+miniShell &miniShell::init(int argc, char *argv[])
+{
+    static miniShell shell{};
+    shell.setPrompt();
+    shell.setPath(argv[0]);
+    return shell;
+}
+
+void miniShell::run()
+{
+    while (true) {
+        getInput();
+        setCommand();
+        executeCommands();
+        if (m_exit) {
+            break;
         }
-        tokens.push_back(nullptr);
-        execvp(tokens[0], tokens.data());
-        perror("execvp error");
-        exit(-1);
     }
-    if (const int wc = wait(nullptr); wc < 0) {
-        perror("wait error");
-        exit(-1);
+}
+
+void miniShell::getInput()
+{
+    printPrompt();
+    getline(cin, m_input);
+}
+
+void miniShell::setCommand()
+{
+    m_commands.clear();
+    std::stringstream ss(m_input);
+    COMMAND_T command;
+    string token;
+    while (ss >> token) {
+        command.push_back(strdup(token.c_str()));
     }
+    command.push_back(nullptr);
+    m_commands.push_back(command);
+}
+
+void miniShell::executeCommands() const
+{
+    for (auto &command: m_commands) {
+        if (m_exit) { return; }
+        executeCommand(command);
+    }
+}
+
+void miniShell::executeCommand(const COMMAND_T &command) const
+{
+    if (m_redirect) {}
+    if (m_pipe) {}
+    int rc = fork();
+    if (rc < 0) { occurError("fork error"); }
+    if (rc == 0) {
+        execvp(command[0], command.data());
+        occurError("execvp error");
+    }
+    else {
+        if (const int wc = wait(&rc); wc < 0) { occurError("wait error"); }
+    }
+}
+
+void miniShell::setPath(string path) { m_path = std::move(path); }
+
+void miniShell::setPrompt(string prompt) { m_prompt = std::move(prompt); }
+
+void miniShell::printPrompt() const { cout << m_path << m_prompt << ' '; }
+
+void miniShell::occurError(const char *message)
+{
+    perror(message);
+    exit(-1);
 }
 
 int main(int argc, char *argv[])
 {
-    while (true) {
-        cout << "wsl:" << argv[0] << "> ";
-        string input;
-        getline(cin, input);
-        if (input == "exit") { break; }
-        call(input);
-    }
+    miniShell &shell = miniShell::init(argc, argv);
+    shell.run();
     return 0;
 }
